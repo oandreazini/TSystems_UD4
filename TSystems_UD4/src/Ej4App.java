1b295ab4bba9cbe16c207e780afd89b9bd99b345
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 12:39:01 +0200

    Fourth Commit

---
+++ TSystems_UD4/src/Ej4App.java
@@ -0,0 +1,25 @@
+
+public class Ej4App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N=40;
+		
+		System.out.println("Valor inicial de N = " +N);
+		
+		int incremento= N+77;
+		
+		System.out.println("El resultado de incrementar N en 77 = " +incremento);
+		
+		int decremento = incremento-3;
+		
+		System.out.println("El resultado de decrementar en 3 = " +decremento);
+		
+		int duplicar = decremento*2;
+		
+		System.out.println("El resultado de duplicar el valor de N = " +duplicar);
+		
+	}
+
+}
