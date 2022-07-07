3368780b90914ab643e6dc4b510ea7a2d97d76e0
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 11:09:13 +0200

    First Commit

---
+++ TSystems_UD4/src/Ej1App.java
@@ -0,0 +1,26 @@
+
+public class Ej1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=20;
+		int b=10;
+		
+		int suma=a+b;
+		System.out.println(suma);
+		
+		int resta=a-b;
+		System.out.println(resta);
+		
+		int multiplica=a*b;
+		System.out.println(multiplica);
+		
+		int division=a/b;
+		System.out.println(division);
+		
+		int resto=a%b;
+		System.out.println(resto);
+	}
+
+}
