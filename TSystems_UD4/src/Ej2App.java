58431e8fd25c1cefff85677c70d081d48ed4819e
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 11:35:44 +0200

    Second Commit

---
+++ TSystems_UD4/src/Ej2App.java
@@ -0,0 +1,25 @@
+    
+public class Ej2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N=10;
+		double A=2.5;
+		char C=102;
+		
+		System.out.println("Variable N = " +N);
+		System.out.println("Variable A = " +A);
+		System.out.println("Variable C = " +C);
+		
+		double suma=N+A;	
+		System.out.println(+N+ " + " +A+ "= " +suma);
+		
+		double resta=A-N;
+		System.out.println(+A+ " + " +N+ "= " +resta);
+		
+		System.out.println("Valor numérico del carácter f = 102");
+		
+	}
+
+}
