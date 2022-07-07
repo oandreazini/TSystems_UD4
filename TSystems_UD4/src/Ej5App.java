c92e6e8df42b2d97f9216dfa9a45505cadc98615
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 12:43:58 +0200

    Fifth Commit

---
+++ TSystems_UD4/src/Ej5App.java
@@ -0,0 +1,26 @@
+
+public class Ej5App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int A=10;
+		int B=20;
+		int C=40;
+		int D=60;
+		
+		B=C;
+		System.out.println("El valor de B actual es = " +B);
+		
+		C=A;
+		System.out.println("El valor de C actual es = " +C);
+		
+		A=D;
+		System.out.println("El valor de A actual es = " +A);
+		
+		D=B;
+		System.out.println("El valor de D actual es = " +D);
+		
+	}
+
+}
