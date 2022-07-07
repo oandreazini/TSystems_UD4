ef31a029b7a83a03aa2978e1313b1f552ba60175
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 12:22:55 +0200

    Third Commit

---
+++ TSystems_UD4/src/Ej3App.java
@@ -0,0 +1,80 @@
+
+public class Ej3App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int X=20;
+		int Y=40;
+		
+		double N=2.5;
+		double M=10.5;
+		
+		int resultado;
+		double resultado1;
+		
+		System.out.println("El resultado X = " +X);
+		System.out.println("El resultado Y = " +Y);
+		System.out.println("El resultado N = " +N);
+		System.out.println("El resultado M = " +M);
+		
+		resultado=X+Y;
+		System.out.println("El resultado X + Y = " +resultado);
+		
+		resultado=X-Y;
+		System.out.println("El resultado X - Y = " +resultado);
+		
+		resultado=X*Y;
+		System.out.println("El resultado X * Y = " +resultado);
+		
+		resultado =X/Y;
+		System.out.println("El resultado X / Y = " +resultado);
+		
+		resultado =X%Y;
+		System.out.println("El resultado X % Y = " +resultado);
+		
+		resultado1=N+M;
+		System.out.println("El resultado N + M = " +resultado1);
+		
+		resultado1=N-M;
+		System.out.println("El resultado N - M = " +resultado1);
+		
+		resultado1=N*M;
+		System.out.println("El resultado N * M = " +resultado1);
+		
+		resultado1=N/M;
+		System.out.println("El resultado N / M = " +resultado1);
+		
+		resultado1=N%M;
+		System.out.println("El resultado N % M = " +resultado1);
+		
+		resultado1=X+N;
+		System.out.println("El resultado X + M = " +resultado1);
+		
+		resultado1=Y/M;
+		System.out.println("El resultado Y / M = " +resultado1);
+		
+		resultado1=Y%M;
+		System.out.println("El resultado Y % M = " +resultado1);
+		
+		resultado=X*2;
+		System.out.println("El resultado del doble de X = " +resultado);
+		
+		resultado=Y*2;
+		System.out.println("El resultado del doble de Y = " +resultado);
+		
+		resultado1=N*2;
+		System.out.println("El resultado del doble de N = " +resultado1);
+		
+		resultado1=M*2;
+		System.out.println("El resultado del doble de M = " +resultado1);
+		
+		resultado1=X+Y+N+M;
+		System.out.println("El resultado de la suma de todas las variables = " +resultado1);
+		
+		resultado1=X*Y*N*M;
+		System.out.println("El resultado del producto de todas las variables = " +resultado1);
+		
+	}
+
+}
